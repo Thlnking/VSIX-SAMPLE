214ed63cb510dc26e96a52aaaa82fee032eee570
Thlnking <2927874032@qq.com>
2022-10-09 23:38:53 +0800

    feat: 添加获取git信息的代码
    Signed-off-by: Thlnking <2927874032@qq.com>

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const fs_1 = require("fs");
+const execSync = require("child_process").execSync;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -12,23 +14,28 @@ function activate(context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "vsix-sample" is now active!');
-  console.log(
-    "%c [ console ]-15",
-    "font-size:13px; background:pink; color:#bf2c9f;",
-    console
-  );
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "vsix-sample.ToolTip",
-    function () {
+    async function () {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
       vscode.window.showInformationMessage("Hello VS Code from vsix-sample!");
-      console.log(vscode.window.activeTextEditor.selections);
+      let name = await execSync("git show -s --format=%cn", {
+        cwd: vscode.workspace.workspaceFolders[0].uri.path,
+      })
+        .toString()
+        .trim(); //姓名
+      let email = await execSync("git show -s --format=%ce", {
+        cwd: vscode.workspace.workspaceFolders[0].uri.path,
+      })
+        .toString()
+        .trim(); //邮箱
+      console.log(name, email);
     }
   );
 
