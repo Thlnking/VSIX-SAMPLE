b4d43378c79f44f3648afa26db5ccb4777a8baf5
Thlnking <2927874032@qq.com>
2022-10-12 16:52:26 +0800

    feat: 增加node执行目录
    Signed-off-by: Thlnking <2927874032@qq.com>

--- src/nvm_status/index.js
+++ src/nvm_status/index.js
@@ -1,4 +1,4 @@
-const { window, StatusBarAlignment } = require("vscode");
+const { window, StatusBarAlignment, workspace } = require("vscode");
 const shell = require("shelljs");
 
 shell.config.execPath = shell.which("node").toString();
@@ -6,12 +6,23 @@ const nvmStatusBarItem = () => {
   // const res = execSync("nvm -v", {
   //   cwd: workspace.workspaceFolders[0].uri.path,
   // }).toString();
-  const nodeVersion = shell.exec("node -v").toString();
 
-  const statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 2);
-  statusBarItem.text = `$(versions) Node ${nodeVersion}`;
-  statusBarItem.tooltip = "";
-  statusBarItem.show();
+  const statusBarItem = window.createStatusBarItem(
+    StatusBarAlignment.Right,
+    -99
+  );
+
+  setInterval(() => {
+    const nodeVersion = shell
+      .exec("node -v", {
+        cwd: workspace.workspaceFolders[0].uri.path,
+      })
+      .toString();
+
+    statusBarItem.text = `$(versions) Node ${nodeVersion}`;
+    statusBarItem.tooltip = "";
+    statusBarItem.show();
+  }, 10 * 1000);
   return statusBarItem;
 };
 
